6c8b267ed582f3d405eee0e4c6202ec6b952fa10
GVE Devnet Admin <gve-devnet.gen@cisco.com>
2023-08-18 16:11:22 +0000

    Baselined from internal Repository last_commit:6dd1ceeb9354b8dab0125f0cfdea0d425031f934

---
+++ README.md
@@ -0,0 +1,81 @@
+# GVE DevNet Catalyst to Meraki Switch Transfer Tool
+This repository contains a Python script that assists in applying a Catalyst switch configuration to a Meraki switch. Specifically, this script will copy the SVI and downlink interface configurations to the Meraki switch. The SVI configuration that gets copied over is the VLAN description/name, interface ip address, and subnet. The interface configuration that gets copied over is the port description/name, data vlan, voice vlan, shutdown status, access/trunk mode, native vlan, and vlans allowed on trunk port. This code is built from the [Catalyst to Meraki Migration Tool](https://github.com/gve-sw/gve_devnet_catalyst_to_meraki_migration) and [Catalyst to Meraki SVI Migration](https://github.com/gve-sw/gve_devnet_meraki_svi_migration).
+
+## Contacts
+* Danielle Stacy
+
+## Solution Components
+* Python 3.8
+* Netmiko
+* Catalyst
+* Meraki
+
+## Prerequisites
+#### Meraki API Keys
+In order to use the Meraki API, you need to enable the API for your organization first. After enabling API access, you can generate an API key. Follow these instructions to enable API access and generate an API key:
+1. Login to the Meraki dashboard
+2. In the left-hand menu, navigate to `Organization > Settings > Dashboard API access`
+3. Click on `Enable access to the Cisco Meraki Dashboard API`
+4. Go to `My Profile > API access`
+5. Under API access, click on `Generate API key`
+6. Save the API key in a safe place. The API key will only be shown once for security purposes, so it is very important to take note of the key then. In case you lose the key, then you have to revoke the key and a generate a new key. Moreover, there is a limit of only two API keys per profile.
+
+> For more information on how to generate an API key, please click [here](https://developer.cisco.com/meraki/api-v1/#!authorization/authorization). 
+
+> Note: You can add your account as Full Organization Admin to your organizations by following the instructions [here](https://documentation.meraki.com/General_Administration/Managing_Dashboard_Access/Managing_Dashboard_Administrators_and_Permissions).
+
+## Installation/Configuration
+1. Clone this repository with `git clone [repository name]`
+2. To complete this step, you will need the API key retrieved in the Prerequisites section. In addition, the code will be connecting to the Catalyst switch via SSH, so you will need to ensure that SSH is configured on the Catalyst switch and know the credentials for the switch. You will also need the serial number of the Meraki switches that you will be configuring from the Catalyst switch. If you are only configuring one switch, then you will only provide one serial number, but if you have a stack or multiple switches that you wish to replace the Catalyst switch with, then you will need to provide all the serial numbers in the appropriate order. Lastly, you'll need the default gateway from the Catalyst configuration. The default gateway for the MS switch must exist on exactly one imported SVI or be configured on  the MS already. Add Catalyst IP address, Catalyst username, Catalyst password, Catalyst ssh secret, Meraki API key, Meraki switch serial(s), and the default gateway ip address to the environment variables
+```python
+IP = "ip address of Catalyst switch"
+USER = "username to ssh to Catalyst switch"
+PASSWORD = "password to ssh to Catalyst switch"
+SECRET = "secret to ssh to Catalyst switch"
+
+API_KEY = "API key for Meraki"
+MS_SERIAL = ["serial num of Meraki switch"]
+
+DEFAULT_GATEWAY = "ip address default gateway for Catalyst switch"
+```
+3. Set up a Python virtual environment. Make sure Python 3 is installed in your environment, and if not, you may download Python [here](https://www.python.org/downloads/). Once Python 3 is installed in your environment, you can activate the virtual environment with the instructions found [here](https://docs.python.org/3/tutorial/venv.html).
+4. Install the requirements with `pip3 install -r requirements.txt`
+
+## Usage
+To run the program, use the command:
+```
+$ python3 config_tool.py
+```
+The script will connect to the Catalyst Switch
+![/IMAGES/step1.png](/IMAGES/step1.png)
+
+Then the script will parse through the SVI configuration of the Catalyst switch
+![/IMAGES/step2.png](/IMAGES/step2.png)
+
+The script will find the shut interfaces on the Catalyst switch
+![/IMAGES/step3.png](/IMAGES/step3.png)
+
+The script will also parse the downlink interface configurations on the Catalyst switch
+![/IMAGES/step4.png](/IMAGES/step4.png)
+
+Last, the script will configure the Meraki switches with the configurations parsed from the Catalyst switch
+![/IMAGES/step5.png](/IMAGES/step5.png)
+
+Then the script will 
+![/IMAGES/0image.png](/IMAGES/0image.png)
+
+### LICENSE
+
+Provided under Cisco Sample Code License, for details see [LICENSE](LICENSE.md)
+
+### CODE_OF_CONDUCT
+
+Our code of conduct is available [here](CODE_OF_CONDUCT.md)
+
+### CONTRIBUTING
+
+See our contributing guidelines [here](CONTRIBUTING.md)
+
+#### DISCLAIMER:
+<b>Please note:</b> This script is meant for demo purposes only. All tools/ scripts in this repo are released for use "AS IS" without any warranties of any kind, including, but not limited to their installation, use, or performance. Any use of these scripts and tools is at your own risk. There is no guarantee that they have been through thorough testing in a comparable environment and we are not responsible for any damage or data loss incurred with their use.
+You are responsible for reviewing and testing any scripts you run thoroughly before use in any non-testing environment.
---
+++ config_tool.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2023 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+from ciscoconfparse import CiscoConfParse
+from netmiko import ConnectHandler
+from dotenv import load_dotenv
+import os
+import ipaddress
+import meraki
+import sys
+import re
+import json
+from pprint import pprint
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import Progress
+
+CONSOLE = Console()
+
+def get_config():
+    """
+    Connect to Catalyst switch over ssh, retrieve show run output
+    :return: string containing results of show run command
+    """
+    # credentials of Catalyst switch to connect to
+    switch = {
+        "device_type": "cisco_ios",
+        "host": os.getenv("IP"),
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD"),
+        "secret": os.getenv("SECRET")
+    }
+
+    CONSOLE.print(f'Connecting to Switch at [green]{switch["host"]}[/]...')
+
+    show_command = f"show run"
+
+    with ConnectHandler(**switch) as connection:
+        # send show run command to switch
+        output = connection.send_command(show_command)
+
+        # check if shout output is valid
+        if "Invalid" in output:
+            CONSOLE.print(f' - [red]Failed to execute "{show_command}"[/], please ensure the command is correct!')
+            return None
+        else:
+            CONSOLE.print(f' - Executed [blue]"{show_command}"[/] successfully!')
+            return output
+
+def parse_svi_data(file_path):
+    """
+    Parse SVI config from show run, extract SVI pieces and build dictionary of SVIs
+    :param file_path: File path to temporary file containing show run output
+    :return: Dictionary of parsed SVIs
+    """
+    # parse config file for VLANs
+    parse = CiscoConfParse(file_path, syntax="ios")
+    vlans = parse.find_objects(r"^interface Vlan")
+
+    vlan_count = len(vlans)
+    CONSOLE.print(f'Found [blue]{vlan_count} vlans[/] to convert!')
+
+    # regex expressions used for finding SVI config details
+    description_re = r"description\s(.+)"
+    ip_address_re = r"ip\saddress\s([\d\.]+)\s([\d\.]+)"
+    ip_helper_address_re = r"ip\shelper-address\s([\d\.]+)"
+
+    # store extracted config info in this dictionary
+    svi_data = {}
+
+    # iterate over VLANs, extract out fields for Meraki API call
+    with Progress() as progress:
+        overall_progress = progress.add_task("Overall Progress", total=vlan_count, transient=True)
+        counter = 1
+
+        for vlan in vlans:
+            # get VLAN ID
+            vlan_id = re.search(r"\d+", vlan.text).group()
+
+            description = ""
+            ip_address = ""
+            subnet_mask = ""
+
+            # search VLAN configuration for description, ip, subnet, helper-address
+            for child in vlan.children:
+                if "description" in child.text:
+                    description = child.re_match_typed(description_re, group=1)
+
+                elif "ip address" in child.text:
+                    ip_address = child.re_match_typed(ip_address_re, group=1)
+                    subnet_mask = child.re_match_typed(ip_address_re, group=2)
+
+            if vlan_id not in svi_data.keys() and ip_address != "" and subnet_mask != "":
+                # convert ip/subnet to cidr notation for API call
+                subnet = ipaddress.ip_network(f"{ip_address}/{subnet_mask}", strict=False)
+
+                svi_data[vlan_id] = {
+                    "name": description if description != "" else "Vlan" + vlan_id,
+                    "vlanId": vlan_id,
+                    "interfaceIp": ip_address,
+                    "subnet": str(subnet),
+                }
+
+                progress.console.print(svi_data[vlan_id])
+            else:
+                # if key fields are missing, skip
+                progress.console.print('[red]Error: one or more key fields are missing from vlan[/] ("ip address"). '
+                                       'Skipping...')
+
+            counter += 1
+            progress.update(overall_progress, advance=1)
+
+    return svi_data
+
+def parse_shut_intf(file_path):
+    """
+    Parse through Catalyst show run output to find which interfaces are shutdown
+    :param file_path: File path to temporary file containing show run output
+    :return: List of names of shut interfaces
+    """
+    CONSOLE.print(f"Checking the number of shut interfaces")
+
+    # parse config file for shutdown interfaces
+    parse = CiscoConfParse(file_path, syntax="ios")
+    shut_interfaces = []
+    for intf_obj in parse.find_objects_w_child("^interface", "^\s+shutdown"):
+        # add interface name of shutdown interface to list
+        shut_interfaces.append(intf_obj.re_match_typed("^interface\s+(\S.+?)$"))
+
+    shut_count = len(shut_interfaces)
+
+    CONSOLE.print(f"Found [blue]{shut_count} shut interfaces[/]")
+
+    return shut_interfaces
+
+def parse_intf_config(file_path, num_switches):
+    """
+    Parse through Catalust show run output to find the interface configurations
+    :param file_path: File path to temporary file containing show run output
+    :param num_switches: Number of Meraki switches that will be configured
+    :return: Dictionary of parsed interfaces
+    """
+    CONSOLE.print(f"Parsing the interface configurations")
+
+    # parse config for interfaces
+    parse = CiscoConfParse(file_path, syntax="ios")
+    interfaces = parse.find_objects("^interface")
+
+    # regex expressions for finding configurations of interfaces
+    description_re = r"description\s(.+)"
+    port_mode_re = r"\sswitchport\smode\s+(.+)"
+    voice_vlan_re = r"\sswitchport\svoice\svlan\s+(\d+)"
+    data_vlan_re = r"\sswitchport\saccess\svlan\s+(\S.*)"
+    trunk_native_re = r"\sswitchport\strunk\snative\svlan\s+(.*)"
+    vlan_allowed_re = r"\sswitchport\strunk\sallowed\svlan\s+(.*)"
+
+    # module_counter is a variable that will keep track of the number of ports on the first switch
+    module_counter = 0
+    # dictionary will contain details of configuration
+    intf_configs = {}
+    # iterate through interfaces and retrieve necessary configurations
+    for intf_obj in interfaces:
+        # find the interface name, switch module, and port number
+        intf_name = intf_obj.re_match_typed("^interface\s+(\S.*)$")
+        only_intf_name = re.sub("\d+|\\/", "", intf_name)
+        switch_module = intf_obj.re_match_typed("^interface\s\S+?thernet+(\d)")
+        port_number = intf_obj.re_match_typed("^interface\s\S+?thernet\d.\d.(\d+)")
+
+        # we only want configuration information of downlink interfaces
+        if only_intf_name.startswith("Giga") and port_number != "":
+            CONSOLE.print(f"Creating [blue] {intf_name}[/] object")
+
+            intf_configs[intf_name] = {}
+            port_number = int(port_number)
+            # if we are only configuring one Meraki switch, we need to make sure the port numbers are correct
+            if num_switches == 1:
+                # the interface is in the first Catalyst switch module
+                if int(switch_module) == 1:
+                    # increase module_counter by 1
+                    module_counter += 1
+                # the interface is not in the first Catalyst switch module (it's part of a stack)
+                elif int(switch_module) > 1:
+                    # adjust port number the corresponding of the corresponding Meraki interface that will be configured
+                    port_number += module_counter * (switch_module - 1)
+            # configuring more than one Meraki switch
+            elif num_switches > 1:
+                # set which Meraki switch needs to be configured in the stack
+                switch_module = int(switch_module)
+                intf_configs[intf_name]["module"] = switch_module
+
+            # search configuration for description, mode, data vlan, voice vlan, native vlan, and allowed vlans
+            intf_configs[intf_name]["portId"] = str(port_number)
+            for child in intf_obj.children:
+                desc = child.re_match_typed(description_re)
+                if desc != "":
+                    intf_configs[intf_name]["name"] = desc
+
+                port_mode = child.re_match_typed(port_mode_re)
+                if port_mode != "":
+                    intf_configs[intf_name]["type"] = port_mode
+
+                voice_vlan = child.re_match_typed(voice_vlan_re)
+                if voice_vlan != "":
+                    intf_configs[intf_name]["voiceVlan"] = voice_vlan
+
+                data_vlan = child.re_match_typed(data_vlan_re)
+                if data_vlan != "":
+                    intf_configs[intf_name]["vlan"] = data_vlan
+
+                trunk_native = child.re_match_typed(trunk_native_re)
+                if trunk_native != "":
+                    intf_configs[intf_name]["vlan"] = trunk_native
+
+                vlan_allowed = child.re_match_typed(vlan_allowed_re)
+                if vlan_allowed != "":
+                    intf_configs[intf_name]["allowedVlans"] = vlan_allowed
+
+
+    intf_count = len(intf_configs)
+    CONSOLE.print(f"Found [blue]{intf_count} downlink interface configurations[/] to convert")
+
+    return intf_configs
+
+def check_default_gateway(meraki_svis):
+    """
+    Determine if a default gateway is already configured for the Meraki switch
+    :parameter meraki_svis: List of already existing SVIs on the Meraki switch
+    :return: Boolean value representing if a default gateway already exists on the Meraki switch
+    """
+    for svi in meraki_svis:
+        if "defaultGateway" in svi:
+            return True
+
+    return False
+
+def create_default_svi(vlan_info, default_gateway, dash, serial):
+    """
+    Set the default gateway vlan of the Meraki switch
+    :param vlan_info: Dictionary containing info of VLAN containing default gateway
+    :param default_gateway: IP address of default gateway
+    :return: String of VLAN ID of the VLAN with the default gateway
+    """
+    # get possible ip addresses of VLAN subnet
+    network = ipaddress.ip_network(vlan_info["subnet"])
+
+    # default gateway in vlan subnet
+    if ipaddress.ip_address(default_gateway) in network:
+        CONSOLE.print(f"Setting the ip default-gateway to {default_gateway}")
+        vlan_info["defaultGateway"] = default_gateway
+
+        svi_response = dash.switch.createDeviceSwitchRoutingInterface(serial, **vlan_info)
+        target_svi_id = vlan_info["vlanId"]
+
+        return target_svi_id
+
+    return None
+
+def configure_meraki(api_key, default_gateway, serials, svi_data, shut_interfaces, intf_configs):
+    """
+    Configure the SVIs and downlink interfaces of the Meraki switches
+    :param api_key: API key for connecting to Meraki dashboard
+    :param default_gateway: String of the IP of the default gateway
+    :param serials: List of Meraki switch serial numbers
+    :param shut_interfaces: List of interfaces that are shutdown
+    :param intf_configs: Dictionary of interfaces and their configurations
+    :return: Nothing
+    """
+    # connect to Meraki dashboard
+    dash = meraki.DashboardAPI(api_key, suppress_logging=True)
+    # iterate through Meraki switch serials and configure the default gateway VLAN
+    default_svi = ""
+    for serial in serials:
+        # get the existing SVI on the Meraki switch
+        meraki_svi_response = dash.switch.getDeviceSwitchRoutingInterfaces(serial)
+
+        meraki_svis = [str(svi["vlanId"]) for svi in meraki_svi_response]
+
+        # if no default gateway on switch, create one
+        if not check_default_gateway(meraki_svis):
+            for vlan_info in svi_data.values():
+                network = ipaddress.ip_network(vlan_info["subnet"])
+
+                if ipaddress.ip_address(default_gateway) in network:
+                    default_svi = create_default_svi(vlan_info, default_gateway, dash, serial)
+
+                    if default_svi is None:
+                        CONSOLE.print(f"[red]Error: {default_gateway} does not exist on SVI. Please ensure Default Gateway exists on exactly one import SVI.[/]")
+                        CONSOLE.print(f"Cannot proceed without Default Gateway. Exiting program...")
+
+                        return
+
+    # remove SVI with default gateway from dictionary
+    if default_svi != "":
+        del svi_data[default_svi]
+    else:
+        CONSOLE.print(f"[red]Error: {default_gateway} does not exist on SVI. Please ensure Default Gateway exists on exactly one import SVI.[/]")
+        CONSOLE.print(f"Cannot proceed without Default Gateway. Exiting program...")
+
+
+    # configure the remaining VLANs on the Meraki switches
+    vlan_count = len(svi_data)
+    sw_count = len(serials)
+    with Progress() as progress:
+        overall_progress = progress.add_task("Overall Progress", total=vlan_count*sw_count, transient=True)
+        vlan_counter = 1
+        for vlan in svi_data:
+            sw_counter = 1
+            for serial in serials:
+                progress.console.print(f"Configuring [bold blue]{vlan}[/] ({vlan_counter} of {vlan_count}) on Meraki switch [blue]{serial}[/] ({sw_counter} of {sw_count})...")
+                svi_response = dash.switch.createDeviceSwitchRoutingInterface(serial, **svi_data[vlan])
+
+                sw_counter += 1
+                progress.update(overall_progress, advance=1)
+
+            vlan_counter += 1
+
+    # configure the downlink interfaces on the Meraki switches
+    intf_count = len(intf_configs)
+    with Progress() as progress:
+        overall_progress = progress.add_task("Overall Progress", total=intf_count, transient=True)
+        intf_counter = 1
+        for intf in intf_configs:
+            progress.console.print(f"Configuring [bold blue]Port {intf_configs[intf]['portId']}[/] on Meraki switch ({intf_counter} of {intf_count})...")
+            if intf in shut_interfaces:
+                intf_configs[intf]["enabled"] = False
+            else:
+                intf_configs[intf]["enabled"] = True
+
+            # if "module" in the keys, then there are multiple switches in the stack
+            if "module" in intf_configs[intf].keys():
+                # switch_num will determine the index of the switch in the list of switches
+                switch_num = intf_configs[intf]["module"]
+                del intf_configs[intf]["module"]
+            else:
+                switch_num = 1
+
+            intf_response = dash.switch.updateDeviceSwitchPort(serials[switch_num-1], **intf_configs[intf])
+            intf_counter += 1
+            progress.update(overall_progress, advance=1)
+
+def main(argv):
+    load_dotenv()
+
+    CONSOLE.print(Panel.fit(f"Cisco Catalyst to Meraki MS Mirgation"))
+
+    # connect to the Catalyst switch, returns the show run output
+    CONSOLE.print(Panel.fit(f"Get Catalyst Configuration (SSH)", title="Step 1"))
+    config = get_config()
+
+    # If config is None, the ssh attempt failed
+    if config is None:
+        print("There was an issue getting the switch configuration. Check the credentials.")
+
+        return
+
+    # write results to temp output file to be parsed
+    temp_file = "temp.txt"
+    with open(temp_file, 'w') as f:
+        f.write(config)
+
+    # get API key from env file
+    api_key = os.getenv("API_KEY")
+    # get default gateway from env file
+    default_gateway = os.getenv("DEFAULT_GATEWAY")
+    # get serials from env file
+    serials = json.loads(os.getenv("MS_SERIAL"))
+
+    # parse out the svi data from the show run configuration
+    CONSOLE.print(Panel.fit(f"Parse SVIs from Cataylst config", title="Step 2"))
+    svi_data = parse_svi_data(temp_file)
+    # parse out the interfaces that are shut from the show run configuration
+    CONSOLE.print(Panel.fit(f"Parse shut interfaces from Catalyst config", title="Step 3"))
+    shut_interfaces = parse_shut_intf(temp_file)
+    # parse out the interface configurations from the show run configuration
+    CONSOLE.print(Panel.fit(f"Parse all interface configurations from Catalyst config", title="Step 4"))
+    interfaces = parse_intf_config(temp_file, len(serials))
+
+    CONSOLE.print(Panel.fit(f"Configure the Meraki switch with the parsed Catalyst interfaces", title="Step 5"))
+    configure_meraki(api_key, default_gateway, serials, svi_data, shut_interfaces, interfaces)
+
+    # delete temp file
+    # os.remove("temp.txt")
+
+    return
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
